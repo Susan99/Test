d42581582ece3c0f870fb3af0d77bb40adcea7b3
shuying <shuying_tongji@126.com>
2015-12-19 15:18:34 +0800

    added hihocoder_001

---
+++ hihocoder/hihocoder_001/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  hihocoder_001
+//
+//  Created by SHUYING on 12/19/15.
+//  Copyright © 2015 SHUYING. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a;
+    int b;
+    while(scanf("%d %d", &a,&b)!= EOF)
+    {
+        printf("%d\n", a+b);
+    }
+    
+    return 0;
+}
